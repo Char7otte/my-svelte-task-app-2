4ea1f9432aa709e826dbc47384df233ac41de33b
shar <faethychan@gmail.com>
2026-05-11 18:04:46 +0800

    refactor: move remote.ts

---
+++ src/lib/remote/user.remote.ts
@@ -0,0 +1,55 @@
+import { form, query } from '$app/server';
+import { sql } from '$lib/server/db/psql';
+import { error } from '@sveltejs/kit';
+import * as z from 'zod';
+import type { User } from '../../types';
+
+const id = z.string().min(1).toLowerCase().trim();
+
+export const getUsers = query(async () => {
+	try {
+		const users = await sql<User[]>`SELECT * FROM users`;
+		if (!users) error(404, 'No users found');
+		return users;
+	} catch {
+		error(500, 'Database connection failed');
+	}
+});
+
+export const getUser = query(id, async (slug: string) => {
+	try {
+		const [user] = await sql<User[]>`SELECT * FROM users WHERE id = ${slug}`;
+		if (!user) error(404, 'No user found');
+		return user;
+	} catch {
+		error(500, 'Database connection failed');
+	}
+});
+
+const user = z.object({
+	email: z.email().toLowerCase().trim(),
+	username: z.string().min(5).toLowerCase().trim(),
+	password: z.string().min(8).trim(),
+	confirmPassword: z.string().min(8).trim()
+});
+
+// export const createUser = form(user, async ({ email, username, password, confirmPassword }) => {
+// 	console.log(email, username, password, confirmPassword);
+// });
+
+export const createUser = form(z.object({ email: z.email() }), async (email) => {
+	console.log('Submitted');
+});
+
+// const user = auth;
+// if (!user) error(404, 'Unauthorized');\
+// const passwordHash = await hashPassword(password);
+// try {
+// 	const [newUser] = await sql<User[]>`INSERT INTO users (email, username, password_hash)
+// 	VALUES(${email}, ${username}, ${passwordHash})
+// 	RETURNING *`;
+// 	// return newUser;
+// 	redirect(303, '/');
+// } catch {
+// 	error(500, 'Database connection failed');
+// }
