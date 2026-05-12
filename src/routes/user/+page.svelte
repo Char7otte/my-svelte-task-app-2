cacbfaceddb613a86bb13d62d50c0a8384a8b347
shar <faethychan@gmail.com>
2026-05-12 16:09:59 +0800

    refactor: break up sign in and sign up forms

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,34 +1,7 @@
 <script lang="ts">
-	import FormIssues from '$lib/formIssues.svelte';
-	import { createUser } from '$lib/remote/user.remote';
-	const { email, username, password, confirmPassword } = createUser.fields;
+	import SignInForm from './signInForm.svelte';
+	import SignUpForm from './signUpForm.svelte';
 </script>
 
-<form
-	class="m-10 space-y-1"
-	{...createUser}
-	onchange={() => createUser.validate()}
->
-	<label class="block"
-		>Email: <br />
-		<input {...email.as('text')} class="border-2 p-1" />
-		<FormIssues issues={email.issues()} />
-	</label>
-	<label class="block"
-		>Username: <br />
-		<input {...username.as('text')} class="border-2 p-1" />
-		<FormIssues issues={username.issues()} />
-	</label>
-	<label class="block"
-		>Password: <br />
-		<input {...password.as('password')} class="border-2 p-1" />
-		<FormIssues issues={password.issues()} />
-	</label>
-	<label class="block"
-		>Confirm Password: <br />
-		<input {...confirmPassword.as('password')} class="border-2 p-1" />
-		<FormIssues issues={confirmPassword.issues()} />
-	</label>
-
-	<button class="mt-3 block border-2 px-1.5 py-1">Sign up</button>
-</form>
+<SignUpForm />
+<SignInForm />
---
+++ src/routes/user/signUpForm.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import FormIssues from '$lib/formIssues.svelte';
+	import { signUp } from '$lib/remote/user.remote';
+
+	const { email, username, password, confirmPassword } = signUp.fields;
+</script>
+
+<form {...signUp} class="m-10 space-y-1" onchange={() => signUp.validate()}>
+	<label class="block"
+		>Email: <br />
+		<input {...email.as('text')} class="border-2 p-1" />
+		<FormIssues issues={email.issues()} />
+	</label>
+	<label class="block"
+		>Username: <br />
+		<input {...username.as('text')} class="border-2 p-1" />
+		<FormIssues issues={username.issues()} />
+	</label>
+	<label class="block"
+		>Password: <br />
+		<input {...password.as('password')} class="border-2 p-1" />
+		<FormIssues issues={password.issues()} />
+	</label>
+	<label class="block"
+		>Confirm Password: <br />
+		<input {...confirmPassword.as('password')} class="border-2 p-1" />
+		<FormIssues issues={confirmPassword.issues()} />
+	</label>
+
+	<button class="mt-3 block border-2 px-1.5 py-1">Sign up</button>
+</form>
