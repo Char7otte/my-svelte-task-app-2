21875dbde8bf8a35c5365336830b23337846e829
shar <faethychan@gmail.com>
2026-05-12 16:15:26 +0800

    feat: toggle between sign in and up

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import SignInForm from './signInForm.svelte';
 	import SignUpForm from './signUpForm.svelte';
+
+	let signOption: 'sign up' | 'sign in' = $state('sign in');
 </script>
 
-<SignUpForm />
-<SignInForm />
+<input type="radio" id="sign-in" value="sign in" bind:group={signOption} />
+<label for="sign-in">Sign in</label>
+<input type="radio" id="sign-up" value="sign up" bind:group={signOption} />
+<label for="sign-up">Sign up</label>
+
+{#if signOption === 'sign in'}
+	<SignInForm />
+{:else}
+	<SignUpForm />
+{/if}
