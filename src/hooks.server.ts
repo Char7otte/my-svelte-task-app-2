a1220483af13230173c15d78d1293836cdb5fb8e
shar <faethychan@gmail.com>
2026-05-12 12:13:52 +0800

    feat: uncomment session handle

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,16 @@
-// import { validateSessionToken } from '$lib/server/auth/authManager';
-// import { getUser } from '$lib/server/remote';
+import { getUser } from '$lib/remote/user.remote';
+import { validateSessionToken } from '$lib/server/auth/authManager';
 
-// export async function handle({ event, resolve }) {
-// 	const token = event.cookies.get('token');
-// 	if (token) {
-// 		const session = await validateSessionToken(token);
-// 		if (session) {
-// 			event.locals.user = await getUser(session.userID);
-// 			event.locals.session = session;
-// 		}
-// 	}
+export async function handle({ event, resolve }) {
+	const token = event.cookies.get('token');
+	if (token) {
+		const session = await validateSessionToken(token);
+		if (session) {
+			event.locals.user = await getUser(session.userID);
+			event.locals.session = session;
+		}
+	}
 
-// 	const response = await resolve(event);
-// 	return response;
-// }
+	const response = await resolve(event);
+	return response;
+}
