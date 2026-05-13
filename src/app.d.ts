710e10778e02472eb475f8cda8e960b31f7e6ea8
shar <faethychan@gmail.com>
2026-05-13 12:33:46 +0800

    refactor: clean up Locals type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,11 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 
-import type { User } from '$lib/types';
-
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			tasks: TaskWithUser;
-			user: User;
-			session: Session;
+			session: SessionClient;
 		}
 		// interface PageData {}
 		// interface PageState {}
