ccd8e088857f9b36444fa7ca3acb5bb954c2e98c
shar <faethychan@gmail.com>
2026-05-13 16:51:04 +0800

    refactor: extract input element

---
+++ src/lib/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import FormIssues from './formIssues.svelte';
+	const { label, type, field } = $props();
+</script>
+
+<label class="block"
+	>{label}: <br />
+	<input {...field.as({ type })} class="border-2 p-1" />
+	<FormIssues issues={field.issues()} />
+</label>
--- src/routes/user/signInForm.svelte
+++ src/routes/user/signInForm.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
-	import FormIssues from '$lib/formIssues.svelte';
+	import Input from '$lib/Input.svelte';
 	import { signIn } from '$lib/remote/user.remote';
 
 	const { email, password } = signIn.fields;
 </script>
 
 <form {...signIn} class="space-y-1" onchange={() => signIn.validate()}>
-	<label class="block"
-		>Email: <br />
-		<input {...email.as('email')} class="border-2 p-1" />
-		<FormIssues issues={email.issues()} />
-	</label>
-	<label class="block"
-		>Password: <br />
-		<input {...password.as('password')} class="border-2 p-1" />
-		<FormIssues issues={password.issues()} />
-	</label>
+	<Input label="Email" type="email" field={email} />
+	<Input label="Password" type="password" field={password} />
 	<button class="mt-3 block border-2 px-1.5 py-1">Sign in</button>
 </form>
