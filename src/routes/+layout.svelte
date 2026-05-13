e299d6a6e349d3fbf82d36e196a6b74cec017b4f
shar <faethychan@gmail.com>
2026-05-13 12:33:23 +0800

    chore: remove code that's being replaced

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import favicon from '$lib/assets/favicon.ico';
-	import { logout } from '$lib/remote/user.remote.js';
 	import './layout.css';
 
 	let { children, data } = $props();
@@ -16,14 +15,6 @@
 <nav class="mb-2 flex space-x-5 bg-gray-200 px-4 py-2 text-lg">
 	<a href={resolve('/')}>Home</a>
 	<a href={resolve('/user')}>User</a>
-	{#if data.user}
-		<div class="flex flex-1 justify-end">
-			<form {...logout}>
-				<input {...logout.fields.sessionID.as('hidden', data.session.id)} />
-				<button>Log out</button>
-			</form>
-		</div>
-	{/if}
 </nav>
 
 <div class="m-10">
