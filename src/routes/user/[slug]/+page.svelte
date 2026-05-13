47f60b63c6dafa340fe4aac450400ff8829b35e5
shar <faethychan@gmail.com>
2026-05-13 16:51:15 +0800

    feat: create edit profile form

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import { logout } from '$lib/remote/user.remote.js';
+
 	const { data } = $props();
 </script>
 
-<h1>viewing {data.id}</h1>
+<h1 class="text-2xl">{data.user.username}</h1>
+<h2>Edit profile</h2>
+<form>
+	<label for="">
+		Username:
+		<input type="text" />
+	</label>
+	<label for="">
+		Original Password:
+		<input type="password" />
+	</label>
+</form>
+
+<button
+	onclick={async () => {
+		await logout(data.session.id);
+		invalidateAll();
+	}}
+	class="float-right">Logout</button
+>
