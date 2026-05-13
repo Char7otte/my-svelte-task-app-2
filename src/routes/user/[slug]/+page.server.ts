cadd03ec366912d796d533f78195ec7b5e677eb2
shar <faethychan@gmail.com>
2026-05-13 16:50:18 +0800

    feat: update user[slug] load

--- src/routes/user/[slug]/+page.server.ts
+++ src/routes/user/[slug]/+page.server.ts
@@ -1,9 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load({ locals, params }) {
-	if (locals.user.id !== params.slug) redirect(308, '/404');
-
-	return {
-		id: params.slug
-	};
+	console.log(locals);
+	if (!locals.user || locals.user.id !== params.slug) redirect(308, '/404');
 }
