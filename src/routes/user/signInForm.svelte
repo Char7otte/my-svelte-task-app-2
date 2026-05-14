31aa1b7e7c7a2b3209da80e95347643d5b0cc3ad
shar <faethychan@gmail.com>
2026-05-14 09:35:49 +0800

    feat: show loading while awaiting form submission

--- src/routes/user/signInForm.svelte
+++ src/routes/user/signInForm.svelte
@@ -3,10 +3,29 @@
 	import { signIn } from '$lib/remote/auth.remote';
 
 	const { email, password } = signIn.fields;
+	let isLoading: boolean = $state(false);
 </script>
 
-<form {...signIn} class="space-y-1" onchange={() => signIn.validate()}>
+<form
+	{...signIn.enhance(async ({ form, submit }) => {
+		try {
+			isLoading = true;
+			if (await submit()) {
+				form.reset();
+			}
+		} catch (e) {
+			throw new Error('Something went wrong in sign in form', { cause: e });
+		} finally {
+			isLoading = false;
+		}
+	})}
+	class="space-y-1"
+>
 	<Input label="Email" type="email" field={email} />
 	<Input label="Password" type="password" field={password} />
 	<button class="mt-3 block border-2 px-1.5 py-1">Sign in</button>
 </form>
+
+{#if isLoading}
+	<p>Signing in...</p>
+{/if}
--- src/routes/user/signUpForm.svelte
+++ src/routes/user/signUpForm.svelte
@@ -3,12 +3,31 @@
 	import { signUp } from '$lib/remote/auth.remote';
 
 	const { email, username, password, confirmPassword } = signUp.fields;
+	let isLoading: boolean = $state(false);
 </script>
 
-<form {...signUp} class="space-y-1" onchange={() => signUp.validate()}>
+<form
+	{...signUp.enhance(async ({ form, submit }) => {
+		try {
+			isLoading = true;
+			if (await submit()) {
+				form.reset();
+			}
+		} catch (e) {
+			throw new Error('Something went wrong in sign up form', { cause: e });
+		} finally {
+			isLoading = false;
+		}
+	})}
+	class="space-y-1"
+>
 	<Input label="Email" type="email" field={email} />
 	<Input label="Username" type="text" field={username} />
 	<Input label="Password" type="password" field={password} />
 	<Input label="Confirm Password" type="password" field={confirmPassword} />
 	<button class="mt-3 block border-2 px-1.5 py-1">Sign up</button>
 </form>
+
+{#if isLoading}
+	<p>Signing in...</p>
+{/if}
