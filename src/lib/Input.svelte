b63bcb3b90cc93aa9fcc1089f80ea02aba1250d1
shar <faethychan@gmail.com>
2026-05-14 11:56:43 +0800

    feat: expand Input component

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import FormIssues from './formIssues.svelte';
-	const { label, type, field } = $props();
+	const {
+		label,
+		type,
+		field,
+		value = '',
+		placeholder = '',
+		children = null,
+		disabled = false
+	} = $props();
 </script>
 
 <label class="block"
 	>{label}: <br />
-	<input {...field.as({ type })} class="border-2 p-1" />
+	<input
+		{...field.as({ type })}
+		class="border-2 p-1"
+		{value}
+		{placeholder}
+		{disabled}
+	/>
+	{@render children()}
 	<FormIssues issues={field.issues()} />
 </label>
