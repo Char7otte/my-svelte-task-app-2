f064e8131bcca339bb18ed90077b4c0cacec0ac3
shar <faethychan@gmail.com>
2026-05-14 10:12:02 +0800

    refactor: create generic form component with await handler

---
+++ src/lib/Form.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let { action, children } = $props();
+	let isLoading: boolean = $state(false);
+	let disabled = $derived(isLoading);
+</script>
+
+<form
+	//@ts-expect-error implicitly typing props? IMGOODBROPIZZAMOVIE.png
+	{...action.enhance(async ({ form, submit }) => {
+		try {
+			isLoading = true;
+			if (await submit()) {
+				form.reset();
+			}
+		} catch (e) {
+			throw new Error('Something went wrong in sign up form', { cause: e });
+		} finally {
+			isLoading = false;
+		}
+	})}
+>
+	<fieldset {disabled} class="disabled:cursor-not-allowed disabled:opacity-50">
+		{@render children()}
+	</fieldset>
+</form>
+
+{#if isLoading}
+	<p>Loading...</p>
+{/if}
--- src/routes/user/signInForm.svelte
+++ src/routes/user/signInForm.svelte
@@ -1,31 +1,13 @@
 <script lang="ts">
+	import Form from '$lib/Form.svelte';
 	import Input from '$lib/Input.svelte';
 	import { signIn } from '$lib/remote/auth.remote';
 
 	const { email, password } = signIn.fields;
-	let isLoading: boolean = $state(false);
 </script>
 
-<form
-	{...signIn.enhance(async ({ form, submit }) => {
-		try {
-			isLoading = true;
-			if (await submit()) {
-				form.reset();
-			}
-		} catch (e) {
-			throw new Error('Something went wrong in sign in form', { cause: e });
-		} finally {
-			isLoading = false;
-		}
-	})}
-	class="space-y-1"
->
+<Form action={signIn}>
 	<Input label="Email" type="email" field={email} />
 	<Input label="Password" type="password" field={password} />
 	<button class="mt-3 block border-2 px-1.5 py-1">Sign in</button>
-</form>
-
-{#if isLoading}
-	<p>Signing in...</p>
-{/if}
+</Form>
