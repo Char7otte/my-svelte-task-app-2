c980e1f1054479eba8e0043698de9e7748c84414
shar <faethychan@gmail.com>
2026-05-14 11:58:17 +0800

    feat: add edit username

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -1,17 +1,41 @@
 <script lang="ts">
-	import Input from "$lib/Input.svelte";
+	import Form from '$lib/Form.svelte';
+	import Input from '$lib/Input.svelte';
+	import { patchUser, patchUserUsername } from '$lib/remote/user.remote';
 
 	const { data } = $props();
+	const { id, username, password, newPassword, confirmNewPassword } =
+		patchUser.fields;
+	let isEditting: boolean = $state(false);
 </script>
 
-<h1 class="text-2xl">{data.selectedUser.username}</h1>
+<h1 class="inline text-2xl">{data.selectedUser.username}</h1>
 
 {#if data.isUser}
-	<form>
-	<Input label="Username" type="email" field={} />
-	<Input label="Current Password" type="password" field={} />
-	<Input label="New Password" type="password" field={} />
-	<Input label="Confirm New Password" type="password" field={} />
-		<button type="button">Edit</button>
-	</form>
+	<button>Edit</button>
+	<Form action={patchUserUsername}>
+		<input {...id.as('hidden', data.user.id)} />
+		<Input
+			label="Username"
+			type="email"
+			field={username}
+			placeholder={data.user.username}
+			value={data.user.username}
+			disabled={!isEditting}
+		>
+			{#if !isEditting}
+				<button type="button" onclick={() => (isEditting = true)}>Edit</button>
+			{:else}
+				<button>Change</button>
+			{/if}</Input
+		>
+	</Form>
 {/if}
+
+<!-- <Input label="Current Password" type="password" field={password} />
+<Input label="New Password" type="password" field={newPassword} />
+<Input
+	label="Confirm New Password"
+	type="password"
+	field={confirmNewPassword}
+/> -->
