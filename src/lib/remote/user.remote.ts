144668c8748f9236fed244c30bf788f6b8979950
shar <faethychan@gmail.com>
2026-05-14 11:56:59 +0800

    feat: create patchUser and patchUserUsername

--- src/lib/remote/user.remote.ts
+++ src/lib/remote/user.remote.ts
@@ -1,8 +1,10 @@
-import { query } from '$app/server';
-import { id } from '$lib/remote/zodSchema';
-import { handleQueryErrors, sql } from '$lib/server/db/psql';
+import { form, query } from '$app/server';
+import { id, password, username } from '$lib/remote/zodSchema';
+import { hashPassword } from '$lib/server/auth/hashUtils';
+import { handleQuery, handleQueryErrors, sql } from '$lib/server/db/psql';
 import type { User } from '$lib/types';
 import { error } from '@sveltejs/kit';
+import z from 'zod';
 
 export const getUser = query(id, async (slug: string) => {
 	try {
@@ -17,3 +19,50 @@ export const getUser = query(id, async (slug: string) => {
 		});
 	}
 });
+
+export const patchUser = form(
+	z
+		.object({
+			id,
+			username,
+			password,
+			newPassword: password,
+			confirmNewPassword: password
+		})
+		.refine(async ({ password, newPassword }) => password !== newPassword, {
+			error: 'New password cannot be old password',
+			path: ['newPassword']
+		})
+		.refine(
+			async ({ newPassword, confirmNewPassword }) =>
+				newPassword === confirmNewPassword,
+			{
+				error: "New passwords don't match",
+				path: ['confirmNewPassword']
+			}
+		),
+	async ({ id, username, newPassword }) => {
+		try {
+			console.log(id, username, newPassword);
+			const newPasswordHash = await hashPassword(newPassword);
+
+			const result = await sql`UPDATE users SET(username, password_hash) 
+				VALUES(${username}, ${newPasswordHash}) 
+				WHERE id = ${id}`;
+			if (result.count !== 1) error(404, 'User not found.');
+		} catch (e) {
+			handleQueryErrors(e);
+		}
+	}
+);
+
+export const patchUserUsername = form(
+	z.object({ id, username }),
+	async ({ id, username }) => {
+		handleQuery(async () => {
+			const result =
+				await sql`UPDATE users SET username = ${username} WHERE id=${id}`;
+			if (result.count !== 1) error(404, 'User not found');
+		});
+	}
+);
